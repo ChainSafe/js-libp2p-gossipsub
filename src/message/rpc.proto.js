ceaea5f103ebecb1b5a86a6b7126b8b94a486892
Mikerah <mikerahqc@protonmail.com>
2019-01-10 00:00:47 -0500

    More preliminary refactoring

---
+++ src/message/rpc.proto.js
@@ -0,0 +1,44 @@
+'use strict'
+module.exports = `
+message RPC {
+  repeated SubOpts subscriptions = 1;
+  repeated Message msgs = 2;
+
+  message SubOpts {
+    optional bool subscribe = 1; // subscribe or unsubcribe
+    optional string topicCID = 2;
+  }
+
+  message Message {
+    optional bytes from = 1;
+    optional bytes data = 2;
+    optional bytes seqno = 3;
+    repeated string topicIDs = 4; 
+  }
+
+  optional ControlMessage control = 3;
+
+  message ControlMessage {
+    repeated ControlIHave ihave = 1;
+    repeated ControlIWant iwant = 2;
+    repeated ControlGraft graft = 3;
+    repeated ControlPrune prune = 4;
+  }
+
+  message ControlIHave {
+    optional string topicID = 1;
+    repeated string messageIDs = 2;
+  }
+
+  message ControlIWant {
+    repeated string messageIDs = 1;  
+  }
+
+  message ControlGraft {
+    optional string topicID = 1;
+  }
+
+  message ControlPrune {
+    optional string topicID = 1;
+  }
+}`
