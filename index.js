b74ecc4ac5e7436db3cbab79838364c98089bc19
Mikerah <mikerahqc@protonmail.com>
2019-01-10 00:00:18 -0500

    started implementation of control message handlers

--- index.js
+++ index.js
@@ -3,26 +3,32 @@
 const libp2p = require('libp2p')
 const EventEmitter = require('events')
 const values = require('lodash/values')
-const pull = require('pul-stream')
+const pull = require('pull-stream')
 const asyncEach = require('async/each')
 const setImmediate = require('async/setImmediate')
 const debug = require('debug')
 
-// Going to need to copy over the message directory from the js floodsub repo
-// and add make changes to the message format for gossipsub
-const rpc = require('/message').rpc.RPC
+const pb = require('./message')
+const MessageCache = require('./messageCache')
+const utils = require('./utils')
+const Peer = require('./peer') // Will switch to js-peer-info once stable
 
-// Going to need to copy over the peer class from the js floodsub repo
-// Don't think we will need to change it since it is pubsub agnostic
-class Peer {
+// Overlay parameters
+const GossipSubD = 6
+const GossipSubDlo = 4
+const GossipSubDhi = 12
 
-}
+// Gossip parameters
+const GossipSubHistoryLength = 5
+const GossipSubHistoryGossip = 3
 
-// Data structure to store messages for gossip in a cache.
-// Will be implemented as in https://github.com/libp2p/go-libp2p-pubsub/blob/master/mcache.go#L15
-class MessageCache {
+// Heartbeat interval
+const GossipSubHeartbeatInitialDelay = 100 // In milliseconds
+const GossipSubHeartbeatInterval = 1 // In seconds
 
-}
+// Fanout ttl
+const GossipSubFanoutTTL = 60 // in seconds
+
 
 class GossipSub extends EventEmitter {
 
@@ -45,14 +51,14 @@ class GossipSub extends EventEmitter {
 	/**
 	 * Map of topic meshes
 	 *
-	 * @type {Map<string, Map<Peer,Set<string>>>}
+	 * @type {Map<string, Set<Peer>>}
 	 */
 	this.mesh = new Map()
 
 	/**
 	 * Map of topics to lists of peers. These mesh peers are peers to which we are publishing to without topic membership
 	 *
-	 *@type {Map<string, Map<Peer, Set<string>>>}
+	 *@type {Map<string, Set<Peer>>}
 	 */
 	this.fanout = new Map()
 
@@ -81,49 +87,98 @@ class GossipSub extends EventEmitter {
 	 * A message cache that contains the messages for last few hearbeat ticks
 	 *
 	 */
-	this.messageCache = new MessageCache()	
-
-	// Dials that are currently in progress
-	this._dials = new Map()
-
-	this._onConnection = this._onConnection.bind(this)
-	this._dialPeer = this._dialPeer.bind(this)
+	this.messageCache = new MessageCache() 
     }
 
 
+    /**
+     * The next few functions will be copied over from the js-libp2p-floodsub
+     * repository. Might need to copy over the base.js file from the js-libp2p-floodsub repository
+     *
+     */
     _addPeer (peer) {
-    
+        const id = peer.info.id.toB58String()
+
+        /*
+          Always use an existing peer.
+          What is happening here is: "If the other peer has already dialed to me, we already have
+          an establish link between the two, what might be missing is a
+          Connection specifically between me and that Peer"
+        */
+        let existing = this.peers.get(id)
+        if (!existing) {
+            this.log('new peer', id)
+            this.peers.set(id, peer)
+            existing = peer
+
+            peer.once('close', () => this._removePeer(peer))
+        }
+        ++existing._references
+
+        return existing
     }
 
     _removePeer (peer) {
+        const id = peer.info.id.toB58String()
+	
+	this.log('remove', id, peer._references)
+	// Only delete when no one else if referencing this peer.
+	if (--peer._references === 0){
+	    this.log('delete peer', id)
+            this.peers.delete(id)
+
+            // Remove this peer from the mesh
+            for(let topic, peers of this.mesh.entries()){
+	        peers.delete(peer)
+	    }
+	    // Remove this peer from the fanout
+            for (let topic, peers of this.fanout.entries()){
+	        peers.delete(peer)
+	    }
+
+	    // Remove from gossip mapping
+            this.gossip.delete(peer)
+	    // Remove from control mapping
+            this.control.delete(peer)
+	}
+    }
+    
+    _onRpc(idB58Str, rpc) {
+        if(!rpc){
+	    return
+	}
+
+	this.log('rpc from', idB58Str)
+	const controlMsg = rpc.control
+	
+	const iwant = this.handleIHave(idB58Str, controlMsg)
+	const ihave = this.handleIWant(idB58Str, controlMsg)
+	const prune = this.handleGraft(idB58Str, controlMsg)
+	this.handlePrune(idB58Str, controlMsg)
+        	
+	
     }
 
-    _dialPeer (peer) {
+    handleIHave(id, controlMsg) {
+    
     }
 
-    _onDial (peerInfo, conn, callback) {
+    handleIWant(id, controlMsg) {
+    
     }
 
-    _onRpc (idB58Str, rpc) {
+    handleGraft(id, controlMsg) {
+    
     }
 
-    _processRpcMessages (msgs) {
+    handlePrune(id, controlMsg) {
+    
     }
 
-    _emitMessages (topics, messages) {
+    _processRpcControlMsgs() {
+    
     }
-
-    _forwardMessages (topics, messages) {
-    }
-
-    _onConnection(protocol, conn) {
-    }
-
-    _processConnection(idB58Str, conn, peer) {
-    }
-
-    _onConnectionEnd(idB58Str, peer, err) {
-    }
+    
 
     /**
      * Mounts the gossipsub protocol onto a libp2p node
