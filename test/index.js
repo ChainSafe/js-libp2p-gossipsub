2436b05c54cb5ad7d82a83381f0466f5ad862391
Mikerah <mikerahqc@protonmail.com>
2019-02-13 16:26:39 -0500

    Renamed tests to test

---
+++ test/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+const GossipSub = require('../src')
+const RPC = require('../src/message').rpc.RPC
+
+
+describe('Basic 2 node tests', () => {
+
+})
+
+describe('Multiple node tests', () => {
+
+})
