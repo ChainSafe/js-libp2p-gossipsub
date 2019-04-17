b5313fd56b7e3b78897d1ab32f9d667786d29160
Mikerah <mikerahqc@protonmail.com>
2019-04-16 22:01:28 -0400

    Rewrote messageCache tests so that each test case is independent of each other

--- test/test_messageCache.js
+++ test/test_messageCache.js
@@ -6,7 +6,8 @@
 const chai = require('chai')
 const dirtyChai = require('dirty-chai')
 chai.use(dirtyChai)
-chai.use('chai-spies')
+const chaiSpies = require('chai-spies')
+chai.use(chaiSpies)
 const expect = chai.expect
 
 const MessageCache = require('../src/messageCache').MessageCache
@@ -14,127 +15,125 @@ const utils = require('../src/utils.js')
 const Buffer = require('buffer').Buffer
 
 const getMsgID = (msg) => {
-    return utils.msgId(msg.from, msg.seqno)
-};
+  return utils.msgId(msg.from, msg.seqno)
+}
 
 describe('Testing Message Cache Operations', () => {
-    let messageCache = new MessageCache(3, 5)
-    let testMessages = []
+  let messageCache = new MessageCache(3, 5)
+  let testMessages = []
 
-    it('Create message cache', () => {
-        const makeTestMessage = (n) => {
-            return {
-                from: 'test' ,
-                data: Buffer.from(n.toString()),
-                seqno: utils.randomSeqno() ,
-                topicIDs:['test']
-            }
-        }
+  before( () => {
+    const makeTestMessage = (n) => {
+      return {
+        from: 'test',
+        data: Buffer.from(n.toString()),
+        seqno: utils.randomSeqno(),
+        topicIDs: ['test']
+      }
+    }
 
-        for(let i=0; i < 60; i++) {
-            testMessages.push(makeTestMessage(i))
-        }
+    for (let i = 0; i < 60; i++) {
+      testMessages.push(makeTestMessage(i))
+    }
 
-        expect(messageCache).to.exist
-        expect(testMessages).to.exist
-        expect(testMessages).to.be.an('array').not.be.empty
+    for (let i = 0; i < 10; i++) {
+      messageCache.put(testMessages[i])
+    }
+  })
 
-    })
+  it('Should retrieve correct messages for each test message', () => {
+    for (let i = 0; i < 10; i++) {
+      let msgId = getMsgID(testMessages[i])
+      let message = messageCache.get(msgId)
+      expect(message).to.equal(testMessages[i])
+    }
+  })
 
-    it('Add messages to message cache', () => {
-        for(let i=0; i < 10; i++) {
-            messageCache.put(testMessages[i])
-            let msgId = getMsgID(testMessages[i])
-            let message = messageCache.get(msgId)
-            expect(message).to.equal(testMessages[i])
-        }
-    })
+  it('Get GossipIDs', () => {
+    let gossipIDs = messageCache.getGossipIDs('test')
+    expect(gossipIDs.length).to.equal(10)
 
-    it('Get GossipIDs', () => {
-        let gossipIDs = messageCache.getGossipIDs('test')
-        expect(gossipIDs.length).to.equal(10)
+    for (let i = 0; i < 10; i++) {
+      let messageID = getMsgID(testMessages[i])
+      expect(messageID).to.equal(gossipIDs[i])
+    }
+  })
 
-        for(let i=0; i< 10; i++) {
-            let messageID = getMsgID(testMessages[i])
-            expect(messageID).to.equal(gossipIDs[i])
-        }
-    })
+  it('Shift message cache', () => {
+    messageCache.shift()
+    for (let i = 10; i < 20; i++) {
+      messageCache.put(testMessages[i])
+    }
 
-    it('Shift message cache', () => {
-        messageCache.shift()
-        for(let i=10; i < 20; i++) {
-            messageCache.put(testMessages[i])
-        }
+    for (let i = 0; i < 20; i++) {
+      let messageID = getMsgID(testMessages[i])
+      let message = messageCache.get(messageID)
+      expect(message).to.equal(testMessages[i])
+    }
 
-        for(let i=0; i < 20; i++) {
-            let messageID = getMsgID(testMessages[i])
-            let message = messageCache.get(messageID)
-            expect(message).to.equal(testMessages[i])
-        }
+    let gossipIDs = messageCache.getGossipIDs('test')
+    expect(gossipIDs.length).to.equal(20)
 
-        let gossipIDs = messageCache.getGossipIDs('test')
-        expect(gossipIDs.length).to.equal(20)
+    for (let i = 0; i < 10; i++) {
+      let messageID = getMsgID(testMessages[i])
+      expect(messageID).to.equal(gossipIDs[10 + i])
+    }
 
-        for(let i=0; i < 10; i++) {
-            let messageID = getMsgID(testMessages[i])
-            expect(messageID).to.equal(gossipIDs[10+i])
-        }
+    for (let i = 10; i < 20; i++) {
+      let messageID = getMsgID(testMessages[i])
+      expect(messageID).to.equal(gossipIDs[i - 10])
+    }
 
-        for(let i=10; i < 20; i++) {
-            let messageID = getMsgID(testMessages[i])
-            expect(messageID).to.equal(gossipIDs[i-10])
-        }
+    messageCache.shift()
+    for (let i = 20; i < 30; i++) {
+      messageCache.put(testMessages[i])
+    }
 
-        messageCache.shift()
-        for(let i=20; i < 30; i++) {
-            messageCache.put(testMessages[i])
-        }
+    messageCache.shift()
+    for (let i = 30; i < 40; i++) {
+      messageCache.put(testMessages[i])
+    }
 
-        messageCache.shift()
-        for(let i=30; i < 40; i++) {
-            messageCache.put(testMessages[i])
-        }
+    messageCache.shift()
+    for (let i = 40; i < 50; i++) {
+      messageCache.put(testMessages[i])
+    }
 
-        messageCache.shift()
-        for(let i=40; i < 50; i++) {
-            messageCache.put(testMessages[i])
-        }
+    messageCache.shift()
+    for (let i = 50; i < 60; i++) {
+      messageCache.put(testMessages[i])
+    }
 
-        messageCache.shift()
-        for(let i=50; i < 60; i++) {
-            messageCache.put(testMessages[i])
-        }
+    expect(messageCache.msgs.size).to.equal(50)
 
-        expect(messageCache.msgs.size).to.equal(50)
+    for (let i = 0; i < 10; i++) {
+      let messageID = getMsgID(testMessages[i])
+      let message = messageCache.get(messageID)
+      expect(message).to.be.an('undefined')
+    }
 
-        for(let i=0; i < 10; i++) {
-            let messageID = getMsgID(testMessages[i])
-            let message = messageCache.get(messageID)
-            expect(message).to.be.an('undefined')
-        }
+    for (let i = 10; i < 60; i++) {
+      let messageID = getMsgID(testMessages[i])
+      let message = messageCache.get(messageID)
+      expect(message).to.equal(testMessages[i])
+    }
 
-        for(let i=10; i < 60; i++) {
-            let messageID = getMsgID(testMessages[i])
-            let message = messageCache.get(messageID)
-            expect(message).to.equal(testMessages[i])
-        }
+    gossipIDs = messageCache.getGossipIDs('test')
+    expect(gossipIDs.length).to.equal(30)
 
-        gossipIDs = messageCache.getGossipIDs('test')
-        expect(gossipIDs.length).to.equal(30)
+    for (let i = 0; i < 10; i++) {
+      let messageID = getMsgID(testMessages[50 + i])
+      expect(messageID).to.equal(gossipIDs[i])
+    }
 
-        for(let i=0; i < 10; i++) {
-            let messageID = getMsgID(testMessages[50+i])
-            expect(messageID).to.equal(gossipIDs[i])
-        }
+    for (let i = 10; i < 20; i++) {
+      let messageID = getMsgID(testMessages[30 + i])
+      expect(messageID).to.equal(gossipIDs[i])
+    }
 
-        for(let i=10; i < 20; i++) {
-            let messageID = getMsgID(testMessages[30+i])
-            expect(messageID).to.equal(gossipIDs[i])
-        }
-
-        for(let i=20; i < 30; i++) {
-            let messageID = getMsgID(testMessages[10+i])
-            expect(messageID).to.equal(gossipIDs[i])
-        }
-    })
+    for (let i = 20; i < 30; i++) {
+      let messageID = getMsgID(testMessages[10 + i])
+      expect(messageID).to.equal(gossipIDs[i])
+    }
+  })
 })
