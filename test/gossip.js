6841dee316c22d8b35b394ea3f5496f6c54398e5
Cayman <caymannava@gmail.com>
2019-05-09 16:49:36 -0500

    Add gossip tests

---
+++ test/gossip.js
@@ -0,0 +1,65 @@
+'use strict'
+/* eslint-env mocha */
+
+const { expect } = require('chai')
+const sinon = require('sinon')
+
+const { GossipSubDhi } = require('../src/constants')
+const {
+  createNode,
+  dialNode,
+  startNode,
+  stopNode
+} = require('./utils')
+
+describe('gossip', () => {
+  let nodes = Array.from({ length: GossipSubDhi + 2 }) // enough nodes to trigger high threshold
+
+  beforeEach(async () => {
+    for (let i = 0; i < nodes.length; i++) {
+      nodes[i] = await createNode('/ip4/127.0.0.1/tcp/0')
+      await startNode(nodes[i])
+      await startNode(nodes[i].gs)
+    }
+  })
+  afterEach(async function () {
+    this.timeout(6000)
+    await Promise.all(nodes.map((n) => stopNode(n.gs)))
+    await Promise.all(nodes.map((n) => stopNode(n)))
+  })
+
+  it('should send gossip to non-mesh peers in topic', async function () {
+    this.timeout(0)
+    const nodeA = nodes[0]
+    const topic = 'Z'
+    // add subscriptions to each node
+    nodes.forEach((n) => n.gs.subscribe(topic))
+    // every node connected to every other
+    for (let i = 0; i < nodes.length - 1; i++) {
+      for (let j = i + 1; j < nodes.length; j++) {
+        await dialNode(nodes[i], nodes[j].peerInfo)
+      }
+    }
+    await new Promise((resolve) => setTimeout(resolve, 500))
+    // await mesh rebalancing
+    await Promise.all(nodes.map((n) => new Promise((resolve) => n.gs.once('gossipsub:heartbeat', resolve))))
+    await new Promise((resolve) => setTimeout(resolve, 500))
+    // set spy
+    sinon.spy(nodeA.gs, 'log')
+
+    nodeA.gs.publish(topic, Buffer.from('hey'))
+    await new Promise((resolve) => nodeA.gs.once('gossipsub:heartbeat', resolve))
+    expect(nodeA.gs.log.callCount).to.be.gt(1)
+    nodeA.gs.log.getCalls()
+      .filter((call) => call.args[0] === 'Add gossip to %s')
+      .map((call) => call.args[1])
+      .forEach((peerId) => {
+        nodeA.gs.mesh.get(topic).forEach((meshPeer) => {
+          expect(meshPeer.info.id.toB58String()).to.not.equal(peerId)
+        })
+      })
+
+    // unset spy
+    nodeA.gs.log.restore()
+  })
+})
