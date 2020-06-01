12c40d1edef0ad3fa44fedee04cad31aca5777e6
Cayman <caymannava@gmail.com>
2020-06-01 15:32:34 -0500

    chore: add more comments to RPC interfaces

--- ts/message/index.ts
+++ ts/message/index.ts
@@ -5,17 +5,57 @@ import protons = require('protons')
 
 const rpcProto = protons(rpcProtoStr)
 
+// These interfaces follow the protobuf definitions in ./rpc.proto.js
+
+/**
+ * Subscription Options
+ */
 export interface SubOpts {
+  /**
+   * Whether to subscribe of unsubscribe
+   * true for subscribe, false for unsubscribe
+   */
   subscribe?: boolean
+  /**
+   * Topic ID
+   */
   topicID?: string
 }
 
+/**
+ * Pubsub message
+ */
 export interface Message {
+  /**
+   * Author of the message
+   *
+   * Note: This is not necessarily the peer who sent the RPC this message is contained in
+   */
   from?: Buffer
+  /**
+   * Opaque blob of data
+   */
   data?: Buffer
+  /**
+   * 64-bit big-endian uint
+   *
+   * No two messages on a topic from the same peer should have the same seqno value
+   */
   seqno?: Buffer
+  /**
+   * Set of topics being published to
+   */
   topicIDs: string[]
+  /**
+   * Signature of the message
+   *
+   * The signature is computed over the marshalled message protobuf excluding the key field
+   * The protobuf bloc is prefixed by the string `libp2p-pubsub:` before signing
+   */
   signature?: Buffer
+  /**
+   * Signing key
+   */
   key?: Buffer
 }
 
@@ -31,23 +71,42 @@ export interface InMessage {
   key?: Buffer
 }
 
+/**
+ * IHAVE control message
+ * Sent to notify a peer with a list of messages that were recently seen by the local router in the included topic id
+ */
 export interface ControlIHave {
   topicID?: string
   messageIDs: string[]
 }
 
+/**
+ * IWANT control message
+ * Sent to request the full content of messages whose IDs were announced by a remote peer in an IHAVE message
+ */
 export interface ControlIWant {
   messageIDs: string[]
 }
 
+/**
+ * GRAFT control message
+ * Sent to notify a peer that it has been added to the local router's mesh for the included topic id
+ */
 export interface ControlGraft {
   topicID?: string
 }
 
+/**
+ * PRUNE control message
+ * Sent to notify a peer that it has been removed from the local router's mesh for the included topic id
+ */
 export interface ControlPrune {
   topicID?: string
 }
 
+/**
+ * Gossip control message container
+ */
 export interface ControlMessage {
   ihave: ControlIHave[]
   iwant: ControlIWant[]
@@ -55,6 +114,9 @@ export interface ControlMessage {
   prune: ControlPrune[]
 }
 
+/**
+ * Gossipsub RPC message
+ */
 export interface RPC {
   subscriptions: SubOpts[]
   msgs: Message[]
