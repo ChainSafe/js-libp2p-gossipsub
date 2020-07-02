a6d30f09d1cd04fb66531e6642b698a614ebaa1a
Cayman <caymannava@gmail.com>
2020-07-01 19:01:48 -0500

    chore: use publish threshold for floodsub peers

--- ts/index.ts
+++ ts/index.ts
@@ -765,7 +765,8 @@ class Gossipsub extends BasicPubsub {
 
           // floodsub peers
           peersInTopic.forEach((peer) => {
-            if (peer.protocols.includes(constants.FloodsubID)) {
+            const score = this.score.score(peer.id.toB58String())
+            if (peer.protocols.includes(constants.FloodsubID) && score >= this._options.scoreThresholds.publishThreshold) {
               tosend.add(peer)
             }
           })
