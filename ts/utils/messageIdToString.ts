ac9c93c51955ae87ce7e8f107c95f91c6527795e
Cayman <caymannava@gmail.com>
2020-10-18 15:42:22 -0500

    Add uint8arrays library

--- ts/utils/messageIdToString.ts
+++ ts/utils/messageIdToString.ts
@@ -1,3 +1,8 @@
+// remove ts-ignore once https://github.com/achingbrain/uint8arrays/pull/4 is merged
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import toString = require('uint8arrays/to-string')
+
 export function messageIdToString (msgId: Uint8Array): string {
-  return (new Uint8Array(msgId)).toString()
+  return toString(msgId, 'base64')
 }
