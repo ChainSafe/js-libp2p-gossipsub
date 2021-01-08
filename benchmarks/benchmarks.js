12626e863075d5fb1ef88cfed36b72c3b70cdf0f
Vasco Santos <vasco.santos@moxy.studio>
2021-01-08 10:03:28 +0100

    chore: update benchmarks (#147)

--- benchmarks/benchmarks.js
+++ benchmarks/benchmarks.js
@@ -8,24 +8,18 @@ const suite = new Benchmark.Suite('gossipsub')
 // Benchmark how many messages we can send from one peer to another
 ;(async function () {
   // create peers
-  const peers = await Promise.all([
-    utils.createNode('/ip4/127.0.0.1/tcp/0'),
-    utils.createNode('/ip4/127.0.0.1/tcp/0')
-  ])
-  // start peer nodes
-  await Promise.all(peers.map((p) => utils.startNode(p)))
-  await Promise.all(peers.map((p) => utils.startNode(p.gs)))
+  const peers = await utils.createGossipsubs({ number: 2 })
   // connect peers
-  await utils.dialNode(peers[0], peers[1].peerInfo)
+  await peers[0]._libp2p.dial(peers[1]._libp2p.peerId)
   // subscribe
-  peers.forEach((p) => p.gs.subscribe('Z'))
+  peers.forEach((p) => p.subscribe('Z'))
   await new Promise((resolve) => setTimeout(resolve, 2000))
 
   // benchmark definition
   suite
     .add('publish and receive', (deferred) => {
-      peers[1].gs.once('Z', (msg) => deferred.resolve(msg))
-      peers[0].gs.publish('Z', new Uint8Array(1024))
+      peers[1].once('Z', (msg) => deferred.resolve(msg))
+      peers[0].publish('Z', new Uint8Array(1024))
     }, {
       defer: true
     })
