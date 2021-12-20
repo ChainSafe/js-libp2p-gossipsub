c7a5f6e7e93efc0533164c34bc9beb0d9394f5ff
Tuyen Nguyen <vutuyen2636@gmail.com>
2021-12-20 15:25:20 +0700

    Enhance SimpleTimeCache to support generic value

--- ts/utils/time-cache.ts
+++ ts/utils/time-cache.ts
@@ -2,13 +2,18 @@ type SimpleTimeCacheOpts = {
   validityMs: number
 }
 
+type CacheValue<T> = {
+  value: T
+  validUntilMs: number
+}
+
 /**
  * This is similar to https://github.com/daviddias/time-cache/blob/master/src/index.js
  * for our own need, we don't use lodash throttle to improve performance.
  * This gives 4x - 5x performance gain compared to npm TimeCache
  */
-export class SimpleTimeCache {
-  private entries: Map<string, number>;
+export class SimpleTimeCache<T> {
+  private entries: Map<string, CacheValue<T>>;
   private validityMs: number;
   private lastPruneTime = 0;
 
@@ -21,8 +26,8 @@ export class SimpleTimeCache {
     }
   }
 
-  put (key: string): void {
-    this.entries.set(key, Date.now())
+  put (key: string, value: T): void {
+    this.entries.set(key, { value, validUntilMs: Date.now() + this.validityMs })
     this.prune()
   }
 
@@ -34,7 +39,7 @@ export class SimpleTimeCache {
     this.lastPruneTime = now
 
     for (const [k, v] of this.entries.entries()) {
-      if (v + this.validityMs < now) {
+      if (v.validUntilMs < now) {
         this.entries.delete(k)
       } else {
         // sort by insertion order
