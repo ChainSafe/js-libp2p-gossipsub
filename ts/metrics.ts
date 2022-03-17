c5f60d76264a95228244888e4e234f1dab371f80
dapplion <35266934+dapplion@users.noreply.github.com>
2022-03-17 14:55:50 +0530

    Clarify metrics names

--- ts/metrics.ts
+++ ts/metrics.ts
@@ -395,8 +395,8 @@ export function getMetrics(
       labelNames: ['reason']
     }),
     /** Total received IHAVE messages by topic */
-    ihaveRcv: register.gauge<{ topic: TopicLabel }>({
-      name: 'gossipsub_ihave_rcv_total',
+    ihaveRcvMsgids: register.gauge<{ topic: TopicLabel }>({
+      name: 'gossipsub_ihave_rcv_msgids_total',
       help: 'Total received IHAVE messages by topic',
       labelNames: ['topic']
     }),
@@ -404,21 +404,21 @@ export function getMetrics(
      *  The number of times we have decided that an IWANT control message is required for this
      *  topic. A very high metric might indicate an underperforming network.
      *  = rust-libp2p `topic_iwant_msgs` */
-    ihaveRcvNotSeenMsg: register.gauge<{ topic: TopicLabel }>({
-      name: 'gossipsub_ihave_rcv_not_seen_msg_total',
+    ihaveRcvNotSeenMsgids: register.gauge<{ topic: TopicLabel }>({
+      name: 'gossipsub_ihave_rcv_not_seen_msgids_total',
       help: 'Total messages per topic we do not have, not actual requests',
       labelNames: ['topic']
     }),
 
     /** Total received IWANT messages by topic */
-    iwantRcv: register.gauge<{ topic: TopicLabel }>({
-      name: 'gossipsub_iwant_rcv_total',
+    iwantRcvMsgids: register.gauge<{ topic: TopicLabel }>({
+      name: 'gossipsub_iwant_rcv_msgids_total',
       help: 'Total received IWANT messages by topic',
       labelNames: ['topic']
     }),
     /** Total requested messageIDs that we don't have */
-    iwantRcvDonthave: register.gauge({
-      name: 'gossipsub_iwant_rcv_dont_have_total',
+    iwantRcvDonthaveMsgids: register.gauge({
+      name: 'gossipsub_iwant_rcv_dont_have_msgids_total',
       help: 'Total requested messageIDs that we do not have'
     }),
     iwantPromiseStarted: register.gauge({
@@ -520,17 +520,17 @@ export function getMetrics(
 
     onIhaveRcv(topicStr: TopicStr, ihave: number, idonthave: number): void {
       const topic = this.toTopic(topicStr)
-      this.ihaveRcv.inc({ topic }, ihave)
-      this.ihaveRcvNotSeenMsg.inc({ topic }, idonthave)
+      this.ihaveRcvMsgids.inc({ topic }, ihave)
+      this.ihaveRcvNotSeenMsgids.inc({ topic }, idonthave)
     },
 
     onIwantRcv(iwantByTopic: Map<TopicStr, number>, iwantDonthave: number): void {
       for (const [topicStr, iwant] of iwantByTopic) {
         const topic = this.toTopic(topicStr)
-        this.iwantRcv.inc({ topic }, iwant)
+        this.iwantRcvMsgids.inc({ topic }, iwant)
       }
 
-      this.iwantRcvDonthave.inc(iwantDonthave)
+      this.iwantRcvDonthaveMsgids.inc(iwantDonthave)
     },
 
     onForwardMsg(topicStr: TopicStr, tosendCount: number): void {
