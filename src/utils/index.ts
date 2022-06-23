4b873a3bcca1501158728d8a201ceb44f8f55017
Franck Royer <franck@status.im>
2022-06-23 10:15:00 +1000

    feat: allow usage of custom codecs
    The gossip codecs are now available on `GossipSub.multicodecs`.
    Hence, the check can use this field instead of a function that uses
    the constants.
    
    This enables library consumers to set their own codecs by assigning a
    different value to `GossipSub.multicodecs`.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './create-gossip-rpc.js'
 export * from './shuffle.js'
-export * from './has-gossip-protocol.js'
 export * from './messageIdToString.js'
 export { getPublishConfigFromPeerId } from './publishConfig.js'
