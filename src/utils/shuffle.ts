67cccb936c5a874267cbd7f2bc6f31e9600f9b5a
Cayman <caymannava@gmail.com>
2025-03-27 17:10:25 -0400

    chore: improve shuffling perf

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,21 +1,27 @@
 /**
  * Pseudo-randomly shuffles an array
  *
+ * If k is specified, only the first k elements of the array are shuffled and returned
+ *
  * Mutates the input array
  */
-export function shuffle<T> (arr: T[]): T[] {
+export function shuffle<T> (arr: T[], k?: number): T[] {
+  // short circuit for trivial array
   if (arr.length <= 1) {
     return arr
   }
-  const randInt = (): number => {
-    return Math.floor(Math.random() * Math.floor(arr.length))
-  }
 
-  for (let i = 0; i < arr.length; i++) {
-    const j = randInt()
+  const n = arr.length
+  const K = Math.min(k ?? Infinity, n)
+  for (let i = 0; i < K; i++) {
+    const j = i + Math.floor(Math.random() * (n - i))
     const tmp = arr[i]
     arr[i] = arr[j]
     arr[j] = tmp
+  }
+
+  if (k !== undefined) {
+    return arr.slice(0, k)
   }
   return arr
 }
